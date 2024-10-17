35ad52a11c92ef64e7a64d3e5cc36569a72e2e2b
Mateus Silva Branco <143047033+MateusBranco02@users.noreply.github.com>
2024-10-17 17:18:31 -0300

    Ajustes no código e estilo

--- prova/src/pages/Lista-jogadores.jsx
+++ prova/src/pages/Lista-jogadores.jsx
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import '../styles/Lista-jogadores.css';
 
 export default function ListaJogadores() {
     const { id } = useParams();
     const [jogadores, setJogadores] = useState([]);
+    const [nomeJogador, setNomeJogador] = useState('');
 
     useEffect(() => {
         listaJogadores(id);
@@ -12,10 +14,11 @@ export default function ListaJogadores() {
 
     const listaJogadores = async (id) => {
         try {
-            const url = `https://api.cartola.globo.com/atletas/pontuados/${id}`;
+            const url = `https://api.cartola.globo.com/atletas/mercado`;
             const response = await axios.get(url);
-            console.log(response.data);
-            setJogadores(response.data);
+            const jogadoresTime = response.data.atletas.filter(jogador => jogador.clube_id === parseInt(id));
+            const filtroJogador = jogadoresTime.filter(jogador => jogador.apelido.toLowerCase().includes(nomeJogador.toLowerCase()));
+            setJogadores(filtroJogador);
         } catch (error) {
             console.error('Erro ao buscar dados dos jogadores!', error);
         }
@@ -23,7 +26,34 @@ export default function ListaJogadores() {
 
     return (
         <>
-            <h1>Lista Jogadores:</h1>
+            <input
+                type="text"
+                placeholder='Buscar Jogador'
+                value={nomeJogador}
+                onChange={(jogador) => setNomeJogador(jogador.target.value)}
+            />
+
+            <button className='btn-buscar' onClick={() => listaJogadores(id)}>Buscar</button>
+
+            <ul>
+                {jogadores.map((jogador) => {
+                    console.log(jogador.foto);
+                    return (
+
+                        <li key={jogador.atleta_id}>
+                            <h2>{jogador.apelido}</h2>
+                            <img
+                                src={jogador.foto
+                                    ? jogador.foto?.replace('FORMATO', '220x220')
+                                    : 'https://s.sde.globo.com/media/person_role/2023/12/06/photo_220x220_eCO6g7f.png'
+                                }
+                                alt={jogador.apelido}
+                            />
+
+                        </li>
+                    );
+                })}
+            </ul>
         </>
     );
 }
--- prova/src/pages/Times.jsx
+++ prova/src/pages/Times.jsx
@@ -6,6 +6,7 @@ import '../styles/Times.css';
 export default function Times() {
     const [nomeTime, setNomeTime] = useState('');
     const [times, setTimes] = useState([]);
+    const [buscarTime, setBuscarTime] = useState([]);
 
     useEffect(() => {
         buscarTimes();
@@ -15,35 +16,53 @@ export default function Times() {
         try {
             const url = `https://api.cartola.globo.com/clubes`;
             const response = await axios.get(url);
-            console.log(response.data);
             setTimes(Object.values(response.data));
+            setBuscarTime(Object.values(response.data));
         } catch (error) {
             console.error('Erro ao buscar dados dos times!', error);
         }
     };
 
+    const filtroTime = () => {
+        const timeFiltrado = times.filter(time => time.nome.toLowerCase().includes(nomeTime.toLocaleLowerCase()));
+        setBuscarTime(timeFiltrado);
+    }
+
     return (
         <>
-            <img className='logo' src={'https://logodownload.org/wp-content/uploads/2017/05/cartola-fc-logo.png'} alt="Logo" />
-            <input
-                type="text"
-                placeholder='Buscar Time'
-                value={nomeTime}
-                onChange={(time) => setNomeTime(time.target.value)}
-            />
-            <button className='btn-buscar' onClick={buscarTimes}>Buscar</button>
+            <div className='container'>
+                <div className='header'>
+                    <img className='logo' src={'https://logodownload.org/wp-content/uploads/2017/05/cartola-fc-logo.png'} alt="Logo" />
+                    <input
+                        className='input-buscar'
+                        type="text"
+                        placeholder='Buscar Time'
+                        value={nomeTime}
+                        onChange={(time) => setNomeTime(time.target.value)}
+                    />
+                    <button className='btn-buscar' onClick={filtroTime}>Buscar</button>
+                </div>
 
-            <h1>Times:</h1>
-
-            <ul>
-                {times.map((time, key) =>
-                    <Link to={`/jogadores/${time.id}`} key={key}>
-                        <li>
-                            <p>{time.nome}</p>
-                        </li>
-                    </Link>
-                )}
-            </ul>
+                <div className='container-times'>
+                    <div>
+                        <ul>
+                            {buscarTime.map((time, key) =>
+                                <Link to={`/jogadores/${time.id}`} key={key}>
+                                    <li>
+                                        <div className='info-times'>
+                                            <img src={time.escudos['60x60']} alt={time.nome} />
+                                            <div className='descricao'>
+                                                <h2>{time.nome}</h2>
+                                                <p className='apelido'>{time.apelido}</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </Link>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
